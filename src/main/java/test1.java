a5c9ca11495e11f87fbc94fc20cffc65fe5fcb38
Rajani Lekkala <rajani.lekkala@gmail.com>
2020-02-10 21:57:40 +0100

    changed test file

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,2 +1,5 @@
 public class test1 {
+    public int negative(int input){
+        return -(input);
+    }
 }
--- src/test/java/test1Test.java
+++ src/test/java/test1Test.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class test1Test {
-
+    test1 t = new test1();
+    @ParameterizedTest
+    @DisplayName("Tests the negative method")
+    @ValueSource(ints = {5,10,15,20,200,1390})
+    void negative_test(int val){
+        assertTrue(t.negative(val) < 0);
+    }
 }
